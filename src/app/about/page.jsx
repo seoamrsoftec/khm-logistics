13204f76f4b765e6d35c62439d6f6474613741a9
Manish-Code-Craft <kumarmanish.amr@gmail.com>
2025-10-29 11:21:06 +0530

    update

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,4 +1,4 @@
-import BannerHeroSlider from "@/components/pages/Home/BannerHeroSlider";
+import HeroBannerCommon from "@/components/pages/Common/HeroBannerCommon";
 
 
 export async function generateMetadata() {
@@ -28,7 +28,12 @@ export async function generateMetadata() {
 export default function About() {
   return (
     <>
-        <BannerHeroSlider/>       
+       <HeroBannerCommon
+          bgImage="/images/about/2.png"
+          title="Trucking Services USA – Reliable Freight & Special Equipment | KHM Logistics" 
+          description="At KHM Logistics, we take pride in delivering dependable and flexible trucking services across the USA. Whether you need standard freight shipping or specialized equipment for unique loads, our experienced team ensures your cargo moves safely, efficiently, and on schedule. Every shipment matters to us — because we handle your freight as if it were our own."         
+          overlayOpacity={0.7}
+       />     
     </>
   );
 }
